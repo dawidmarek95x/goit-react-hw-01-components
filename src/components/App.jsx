8750b4df7993e9c9d9f9c71af1ab858e5ef44862
dawidmarek95x <dawid.marek95x@gmail.com>
2022-08-03 02:47:47 +0200

    structure of Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import { Profile } from "./Profile/Profile";
 import user from './Profile/user.json';
+import { Statistics } from "./Statistics/Statistics";
+import data from './Statistics/data.json';
 
 export const App = () => {
   const {username, tag, location, avatar, stats} = user;
@@ -12,6 +14,10 @@ export const App = () => {
         location={location}
         avatar={avatar}
         stats={stats}
+      />
+      <Statistics 
+        title="Upload stats" 
+        stats={data}
       />
     </>
   );
