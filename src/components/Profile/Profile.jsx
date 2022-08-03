c95df89c66317e2ab4234a271a203bc9f8a56f35
dawidmarek95x <dawid.marek95x@gmail.com>
2022-08-03 02:14:14 +0200

    styles of Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,34 +1,35 @@
 import PropTypes from "prop-types";
-// import styles from './Profile.module.scss';
+import styles from './Profile.module.scss';
 
 export const Profile = ({username, tag, location, avatar, stats}) => {
   const {followers, views, likes} = stats;
+  const {profile, profile__description, profile__avatar, profile__name, profile__tag, profile__location, profile__stats, counter, label, quantity} = styles;
 
   return (
-    <div className="profile">
-      <div className="description">
+    <div className={profile}>
+      <div className={profile__description}>
         <img
           src={avatar}
           alt="User avatar"
-          className="avatar"
+          className={profile__avatar}
         />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+        <p className={profile__name}>{username}</p>
+        <p className={profile__tag}>@{tag}</p>
+        <p className={profile__location}>{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers ? followers : 0}</span>
+      <ul className={profile__stats}>
+        <li className={counter}>
+          <span className={label}>Followers</span>
+          <span className={quantity}>{followers ? followers : 0}</span>
         </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views ? views : 0}</span>
+        <li className={counter}>
+          <span className={label}>Views</span>
+          <span className={quantity}>{views ? views : 0}</span>
         </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes ? likes : 0}</span>
+        <li className={counter}>
+          <span className={label}>Likes</span>
+          <span className={quantity}>{likes ? likes : 0}</span>
         </li>
       </ul>
     </div>
