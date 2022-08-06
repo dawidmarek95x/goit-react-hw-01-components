3266d4781c312e7622d3cb126c958b4dfef4f7f3
dawidmarek95x <dawid.marek95x@gmail.com>
2022-08-06 21:54:13 +0200

    code-refactoring

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -2,15 +2,13 @@ import PropTypes from "prop-types";
 import styles from './TransactionHistory.module.scss';
 
 export const TransactionHistory = ({items}) => {
-  const {transactions, headers__row, headers, headers__type, data__row, data, data__type} = styles;
-
-  const headersType = `${headers} ${headers__type}`
+  const {transactions, headers__row, headers, data__row, data, data__type} = styles;
 
   return (
     <table className={transactions}>
       <thead>
         <tr className={headers__row}>
-          <th className={headersType}>Type</th>
+          <th className={headers}>Type</th>
           <th className={headers}>Amount</th>
           <th className={headers}>Currency</th>
         </tr>
